50d54a612f38c951ea650ef6d402f8e20cd99a03
vasyoid <vasyoid@gmail.com>
2021-08-04 19:49:15 +0300

    Add latest HelloWorld and Helper test classes

--- substratevm/HelloWorld.java
+++ substratevm/HelloWorld.java
@@ -3,6 +3,166 @@ import java.util.*;
 class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println(Helper.inc(10));
+		f1();
+		f2();
+		ConcreteClass cc = new ConcreteClass();
+		System.out.println(cc.virtualMethod(1));
+		AbstractClass ac = cc;
+		System.out.println(ac.virtualMethod(2));
+		System.out.println(plus(1, 2, 3));
+	}
+
+	public static void f1() {
+		if (getRandom()) {
+			smallMethod();
+		} else {
+			bigMethod();
+		}
+	}
+
+	public static void f2() {
+		while (getRandom()) {
+			smallMethod();
+		}
+		bigMethod();
+	}
+
+	static volatile int fortyTwo = 42;
+
+	static int plus(int x, int y, int z) {
+		int result = x + fortyTwo;
+		result = y + fortyTwo;
+		return result;
+	}
+
+	private static boolean getRandom() {
+		Random random = new Random();
+		return random.nextBoolean();
+	}
+
+	private static int smallMethod() {
+		return 42;
+	}
+
+	private static void bigMethod() {
 		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+		System.out.println("Hello world");
+	}
+
+	static abstract class AbstractClass {
+		abstract int virtualMethod(int x);
+	}
+
+	static class ConcreteClass extends AbstractClass {
+		@Override
+		int virtualMethod(int x) {
+			final int fortyOne = 41;
+			return x + 41;
+		}
 	}
 }
---
+++ substratevm/Helper.java
@@ -0,0 +1,5 @@
+public class Helper {
+	public static int inc(int x) {
+		return x + 1;
+	}
+}
