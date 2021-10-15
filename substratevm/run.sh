f4635ea8d2b2b17eec7fb17f6d4a8d349d163509
IAmFunkyFrog <st085873@student.spbu.ru>
2021-10-15 22:05:52 +0300

    Added easier way to load prototype

--- substratevm/run.sh
+++ substratevm/run.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-GRAAL_PATH=/home/stepan-trefilov/IdeaProjects/graal/sdk/mxbuild/linux-amd64/GRAALVM_CE0C781D19_JAVA8/graalvm-ce0c781d19-java8-21.2.0-dev
+source ./config
 
-RTJAR_1_PATH="/home/stepan-trefilov/openjdk1.8.0_252-jvmci-20.1-b02/jre/lib/rt.jar"
-RTJAR_2_PATH="/home/stepan-trefilov/openjdk1.8.0_302-jvmci-20.3-b22/jre/lib/rt.jar"
 REMOVE_IDENTITIES=false
-TEST_CLASS="HelloWorld"
-DUMP_DIR="graal_dumps/compare"
 
 TEST_CLASS_LOWER_CASE=$(echo "$TEST_CLASS" | tr '[:upper:]' '[:lower:]')
 
 run_on_jar () {
-	mv "$GRAAL_PATH/jre/lib/rt.jar" "$GRAAL_PATH/jre/lib/rt.jar-backup"
-	cp "$1" "$GRAAL_PATH/jre/lib/"
+	mv "$GRAAL_BUILDED_JDK/jre/lib/rt.jar" "$GRAAL_BUILDED_JDK/jre/lib/rt.jar-backup"
+	cp "$1" "$GRAAL_BUILDED_JDK/jre/lib/"
 
-	$GRAAL_PATH/jre/bin/java \
+	$GRAAL_BUILDED_JDK/jre/bin/java \
 	$( [[ $3 == debug ]] && printf %s '-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8000' ) \
 	-XX:+UseParallelGC \
 	-XX:+UnlockExperimentalVMOptions \
@@ -37,21 +33,21 @@ run_on_jar () {
 	-Dcom.oracle.graalvm.isaot=true \
 	-Djava.system.class.loader=com.oracle.svm.hosted.NativeImageSystemClassLoader \
 	-Xshare:off \
-	-Djvmci.class.path.append=$GRAAL_PATH/jre/lib/jvmci/graal.jar \
+	-Djvmci.class.path.append=$GRAAL_BUILDED_JDK/jre/lib/jvmci/graal.jar \
 	-Djdk.internal.lambda.disableEagerInitialization=true \
 	-Djdk.internal.lambda.eagerlyInitialize=false \
 	-Djava.lang.invoke.InnerClassLambdaMetafactory.initializeLambdas=false \
-	-Dllvm.bin.dir=/home/stepan-trefilov/IdeaProjects/graal/sdk/mxbuild/linux-amd64/LLVM_TOOLCHAIN/bin/ \
-	-javaagent:$GRAAL_PATH/jre/lib/svm/builder/svm.jar \
-	-Xbootclasspath/a:$GRAAL_PATH/jre/lib/boot/graal-sdk.jar \
+	-Dllvm.bin.dir=$GRAAL_PATH/sdk/mxbuild/linux-amd64/LLVM_TOOLCHAIN/bin/ \
+	-javaagent:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/svm.jar \
+	-Xbootclasspath/a:$GRAAL_BUILDED_JDK/jre/lib/boot/graal-sdk.jar \
 	-cp \
-	$GRAAL_PATH/jre/lib/svm/builder/objectfile.jar:$GRAAL_PATH/jre/lib/svm/builder/pointsto.jar:$GRAAL_PATH/jre/lib/svm/builder/javacpp-shadowed.jar:$GRAAL_PATH/jre/lib/svm/builder/llvm-platform-specific-shadowed.jar:$GRAAL_PATH/jre/lib/svm/builder/llvm-wrapper-shadowed.jar:$GRAAL_PATH/jre/lib/svm/builder/svm-llvm.jar:$GRAAL_PATH/jre/lib/svm/builder/svm.jar:$GRAAL_PATH/jre/lib/jvmci/jvmci-api.jar:$GRAAL_PATH/jre/lib/jvmci/graal-management.jar:$GRAAL_PATH/jre/lib/jvmci/graal.jar:$GRAAL_PATH/jre/lib/jvmci/graal-truffle-jfr-impl.jar:$GRAAL_PATH/jre/lib/jvmci/jvmci-hotspot.jar:$GRAAL_PATH/jre/lib/resources.jar \
+	$GRAAL_BUILDED_JDK/jre/lib/svm/builder/objectfile.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/pointsto.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/javacpp-shadowed.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/llvm-platform-specific-shadowed.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/llvm-wrapper-shadowed.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/svm-llvm.jar:$GRAAL_BUILDED_JDK/jre/lib/svm/builder/svm.jar:$GRAAL_BUILDED_JDK/jre/lib/jvmci/jvmci-api.jar:$GRAAL_BUILDED_JDK/jre/lib/jvmci/graal-management.jar:$GRAAL_BUILDED_JDK/jre/lib/jvmci/graal.jar:$GRAAL_BUILDED_JDK/jre/lib/jvmci/graal-truffle-jfr-impl.jar:$GRAAL_BUILDED_JDK/jre/lib/jvmci/jvmci-hotspot.jar:$GRAAL_BUILDED_JDK/jre/lib/resources.jar \
 	com.oracle.svm.hosted.NativeImageGeneratorRunner \
 	-imagecp \
-	$GRAAL_PATH/jre/lib/boot/graal-sdk.jar:/home/stepan-trefilov/IdeaProjects/graal/substratevm:$GRAAL_PATH/jre/lib/svm/library-support.jar \
+	$GRAAL_BUILDED_JDK/jre/lib/boot/graal-sdk.jar:$GRAAL_PATH/substratevm:$GRAAL_BUILDED_JDK/jre/lib/svm/library-support.jar \
 	-H:Path="$(pwd)" \
-	-H:CLibraryPath=/home/stepan-trefilov/IdeaProjects/graal/substratevm/mxbuild/linux-amd64/SVM_HOSTED_NATIVE/linux-amd64 \
-	-H:CLibraryPath=$GRAAL_PATH/jre/lib/svm/clibraries/linux-amd64 \
+	-H:CLibraryPath=$GRAAL_PATH/substratevm/mxbuild/linux-amd64/SVM_HOSTED_NATIVE/linux-amd64 \
+	-H:CLibraryPath=$GRAAL_BUILDED_JDK/jre/lib/svm/clibraries/linux-amd64 \
 	"-H:Class@explicit main-class=$TEST_CLASS" \
 	"-H:Name@main-class lower case as image name=$TEST_CLASS_LOWER_CASE" \
 	-H:-AOTInline \
@@ -62,7 +58,7 @@ run_on_jar () {
 	$( [[ $REMOVE_IDENTITIES == false ]] && printf %s "-H:-CanonicalGraphStringsRemoveIdentities" ) \
 	-H:DumpPath="$DUMP_DIR/$2" \
 
-	mv "$GRAAL_PATH/jre/lib/rt.jar-backup" "$GRAAL_PATH/jre/lib/rt.jar"
+	mv "$GRAAL_BUILDED_JDK/jre/lib/rt.jar-backup" "$GRAAL_BUILDED_JDK/jre/lib/rt.jar"
 }
 
 run_on_jar $RTJAR_1_PATH 1 no_debug
